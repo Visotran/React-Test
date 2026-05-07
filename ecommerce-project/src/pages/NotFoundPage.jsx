a607ab7576d5b8d0206c62458e21f26557c00aa8
Tim Glaetzer <timsupercoc@gmail.com>
2026-05-07 22:28:17 +0200

    Completed the rest of the excercises of lesson 6

---
+++ ecommerce-project/src/pages/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+import {Header} from '../components/Header'
+import './NotFoundPage.css';
+
+export function NotFoundPage() {
+  return (
+    <>
+      <Header></Header>
+      <div className="not-found-container">
+        <p className="not-found-text">The page you're looking for could not be found.</p>
+      </div>
+    </>
+  );
+}
