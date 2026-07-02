f416d6b018fe4c37d88438752ff217fd3c15b5a5
Tim Glaetzer <timsupercoc@gmail.com>
2026-07-02 22:03:18 +0200

    Added tests for clicking the Add to cart button

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -73,7 +73,7 @@ export function Product({product, loadCart}) {
         Added
       </div> 
 
-      <button onClick={addToCart} className="add-to-cart-button button-primary">
+      <button onClick={addToCart} className="add-to-cart-button button-primary" data-testid="add-to-cart-button">
         Add to Cart
       </button>
     </div>
