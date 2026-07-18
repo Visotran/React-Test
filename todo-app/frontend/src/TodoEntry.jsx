fc9753742a945ad2d8d9095ae4cc6f48797deb36
Tim Glaetzer <timsupercoc@gmail.com>
2026-07-18 19:59:21 +0200

    Changed the deadlines to be padded with starting zeros

--- todo-app/frontend/src/TodoEntry.jsx
+++ todo-app/frontend/src/TodoEntry.jsx
@@ -1,11 +1,17 @@
 import "./TodoEntry.css";
 import penIcon from "./assets/pen.png"
 
-function TodoEntry({todo, onDelete, onEdit}) {
-  return ( 
+function TodoEntry({ todo, onDelete, onEdit }) {
+  return (
     <div className="todo-entry-container">
       <p className="todo-entry-name">{todo.name}</p>
-      <p className="todo-entry-deadline">{new Date(todo.deadline).toLocaleString()}</p>
+      <p className="todo-entry-deadline">{new Date(todo.deadline).toLocaleString(undefined, {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute:"2-digit"
+      })}</p>
       <button className="delete-button" onClick={() => onDelete(todo)}>
         X
       </button>
